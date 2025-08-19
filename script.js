cc897c822905371c1bc8b79c1c14591ae0aa106a
Padma-darsi <125295009+Padma-darsi@users.noreply.github.com>
2025-08-19 19:39:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,27 @@ function mincost(arr)
 { 
 //write your code here
 // return the min cost
+	function mincost(arr) {
+  if (arr.length === 1) return 0;
+
+  const heap = arr.slice();         // copy the array
+  heap.sort((a, b) => a - b);       // [2, 3, 4, 6]
+
+  let totalCost = 0;
+
+  while (heap.length > 1) {
+    const first = heap.shift();     // remove smallest
+    const second = heap.shift();    // remove next smallest
+
+    const cost = first + second;
+    totalCost += cost;
+
+    heap.push(cost);                // insert new rope
+    heap.sort((a, b) => a - b);     // re-sort
+  }
+
+  return totalCost;
+
   
 }
 
